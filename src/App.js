195455db254c11e6505289595b4229e034c14269
Artem <aprricod@gmail.com>
2021-05-15 10:12:03 +0300

    добавлены кнопка и каунтер

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.PureComponent {
+  counter = 0;
+  constructor(props) {
+    super(props);
+    this.press = this.press.bind(this);
+  }
+
+  press() {
+    this.counter++;
+    console.log(this.counter, "!!!");
+  }
+  render() {
+    const { name } = this.props;
+    return (
+      <div className="App">
+        <p>Hello world {name}</p>
+        <button onClick={this.press}>Кнопка</button>
+      </div>
+    );
+  }
 }
 
 export default App;
